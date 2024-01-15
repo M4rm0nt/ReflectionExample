7805f63cd43407f2a9ce08e5193db4447c3600a0
Dietmar Darimont <dietmar.darimont@gmail.com>
2024-01-16 00:11:25 +0100

    Beispiel verbessert

--- src/main/java/ReflectionExample.java
+++ src/main/java/ReflectionExample.java
@@ -2,46 +2,49 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
 
 public class ReflectionExample {
     public static void main(String[] args) {
 
         try {
-
-            Class<MyClass> clazz = MyClass.class;
+            Class<Mensch> clazz = Mensch.class;
             System.out.println("\nKlassenname: " + clazz.getName());
 
             Constructor<?>[] constructors = clazz.getConstructors();
             System.out.println("\nKonstruktoren: ");
-
             for (Constructor<?> constructor : constructors) {
                 System.out.println(constructor);
             }
 
-            Method[] methods = clazz.getMethods();
+            Method[] methods = clazz.getDeclaredMethods();
             System.out.println("\nMethoden: ");
-
             for (Method method : methods) {
                 System.out.println(method);
+                if (method.getParameterCount() == 0) {
+                    method.setAccessible(true);
+                    method.invoke(clazz.getDeclaredConstructor().newInstance());
+                }
             }
 
             Field[] fields = clazz.getDeclaredFields();
             System.out.println("\nFelder: ");
-
             for (Field field : fields) {
                 System.out.println(field);
             }
 
-            MyClass instance = clazz.getDeclaredConstructor().newInstance();
+            Mensch instance = clazz.getDeclaredConstructor().newInstance();
             System.out.println("\nInstanz erstellt: " + instance);
 
-            Field field = clazz.getDeclaredField("myField");
-            field.setAccessible(true);
-            field.setInt(instance, 10);
-            System.out.println("\nGeänderter Feldwert: " + field.get(instance));
+            Field fieldAlter = clazz.getDeclaredField("alter");
+            fieldAlter.setAccessible(true);
+            fieldAlter.setInt(instance, 43);
 
-            Method method = clazz.getMethod("myMethod");
-            method.invoke(instance);
+            Field fieldName = clazz.getDeclaredField("name");
+            fieldName.setAccessible(true);
+            fieldName.set(instance, "Max");
+
+            System.out.println("\n" + "Instanzfelder geändert: " + instance);
 
         } catch (IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException | NoSuchFieldException e) {
             e.printStackTrace();
@@ -49,13 +52,44 @@ public class ReflectionExample {
     }
 }
 
-class MyClass {
-    private int myField;
+class Mensch {
+    private int alter;
+    private String name;
 
-    public MyClass() {
+    public Mensch() {
     }
 
-    public void myMethod() {
-        System.out.println("myMethod wurde aufgerufen.");
+    public Mensch(int alter, String name) {
+        this.alter = alter;
+        this.name = name;
+    }
+
+    private void myAlter() {
+        System.out.println("myAlter wurde aufgerufen.");
+    }
+
+    private void myName() {
+        System.out.println("myName wurde aufgerufen.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mensch mensch = (Mensch) o;
+        return alter == mensch.alter && Objects.equals(name, mensch.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(alter, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Mensch{" +
+                "alter=" + alter +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
